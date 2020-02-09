baf41d1a0172538f5f840527611a0c1ea809aca1
Aleksey <alekcyvolkov@gmail.com>
2020-02-09 19:36:01 +0300

    feat: add store, change search box component

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import {
 	OutlinedInput,
 	InputLabel,
@@ -11,51 +11,63 @@ import { Search } from '@material-ui/icons';
 import Autocomplete from 'react-autocomplete';
 import { SearchBoxContainer } from './SearchBoxContainer';
 import { sendSearchAddressRequest } from "../../api/index";
+import { store } from "../../store/store";
+import { SET_ADDRESS } from "../../store/actionTypes";
 
 export const SearchBox = () => {
+	const globalState = useContext(store);
 	const [searchText, setSearchText] = useState('');
-	const [items, setItems] = useState([]);
+	const [addressList, setAddressList] = useState([]);
 	const onChange = e => {
 		setSearchText(e.target.value);
-		sendSearchAddressRequest(e.target.value).then(response => {
-			setItems(response.results);
-		})
+		if (e.target.value.length > 3) {
+			sendSearchAddressRequest(e.target.value)
+				.then(response => setAddressList(response.results));
+		}
+	};
+	const onSelect = value => {
+		setSearchText(value);
+		const address = addressList.find(item => item.formatted_address === value);
+		globalState.dispatch({ type: SET_ADDRESS, address: address });
 	};
 	return (
 		<SearchBoxContainer fullWidth>
 			<Autocomplete
+				value={searchText}
+				onChange={onChange}
+				onSelect={onSelect}
 				getItemValue={(item) => item.formatted_address}
-				items={items}
-				renderInput={props => (
-					<FormControl variant="outlined" fullWidth>
-						<InputLabel htmlFor="outlined-address">Address</InputLabel>
-						<OutlinedInput
-							id="outlined-address"
-							endAdornment={
-								<InputAdornment position="end">
-									<IconButton
-										aria-label="Search"
-										onClick={() => {}}
-										edge="end"
-									>
-										<Search />
-									</IconButton>
-								</InputAdornment>
-							}
-							labelWidth={70}
-							{...props}
-						/>
-					</FormControl>
-				)}
+				items={addressList}
+				renderInput={props => {
+					const { ref, ...rest } = props;
+					return (
+						<FormControl variant="outlined" fullWidth>
+							<InputLabel htmlFor="outlined-address">Address</InputLabel>
+							<OutlinedInput
+								{...rest} inputRef={ref}
+								id="outlined-address"
+								endAdornment={
+									<InputAdornment position="end">
+										<IconButton
+											aria-label="Search"
+											onClick={() => {}}
+											edge="end"
+										>
+											<Search />
+										</IconButton>
+									</InputAdornment>
+								}
+								labelWidth={70}
+							/>
+						</FormControl>
+					)
+				}}
 				wrapperStyle={{ width: '100%' }}
 				renderItem={(item, isHighlighted) =>
-					<div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+					<div key={item.id} style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
 						{item.formatted_address}
 					</div>
 				}
-				value={searchText}
-				onChange={onChange}
-				onSelect={val => setSearchText(val)}
 			/>
 		</SearchBoxContainer>
 	)
---
+++ src/store/store.js
@@ -0,0 +1,26 @@
+import React, {createContext, useReducer} from 'react';
+import { SET_ADDRESS } from './actionTypes';
+
+const initialState = {
+	addressStore: null
+};
+const store = createContext(initialState);
+const { Provider } = store;
+
+const StateProvider = ({ children }) => {
+	const [state, dispatch] = useReducer((state, action) => {
+		switch(action.type) {
+			case SET_ADDRESS:
+				return {
+					...state,
+					addressStore: action.address
+				};
+			default:
+				throw new Error();
+		};
+	}, initialState);
+
+	return <Provider value={{ state, dispatch }}>{children}</Provider>;
+};
+
+export { store, StateProvider }
